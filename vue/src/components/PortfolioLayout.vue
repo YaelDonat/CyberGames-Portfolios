80612d9732d20709aaf6101afaeb70bdc89466f6
YaelDonat <donatyael@gmail.com>
2022-03-06 16:47:00 +0100

    portfolio layout test

--- vue/src/components/PortfolioLayout.vue
+++ vue/src/components/PortfolioLayout.vue
@@ -1,13 +1,113 @@
 <template>
-          <router-view></router-view>
+  <div class="min-h-full">
+    <Disclosure as="nav" class="bg-violet-500" v-slot="{ open }">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-center h-16">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-8 w-8" src="../assets/images/cybericon.svg" alt="Workflow" />
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-center space-x-4">
+                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-violet-900 text-white' : 'text-white hover:bg-violet-700 hover:text-white', 'px-3 py-2 rounded-md text-md font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              </div>
+            </div>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-center md:ml-6">
+              
+
+              <!-- Profile dropdown -->
+              <Menu as="div" class="ml-3 relative">
+                <div>
+                  <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span class="sr-only">Menu utilisateur</span>
+                  </MenuButton>
+                </div>
+                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                  <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </div>
+          </div>
+          <div class="-mr-2 flex md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span class="sr-only">Menu principal</span>
+              <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel class="md:hidden">
+        <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        </div>
+        <div class="pt-4 pb-3 border-t border-gray-700">
+          <div class="flex items-center px-5">
+            <div class="flex-shrink-0">
+            </div>
+            <button type="button" class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+
+    <!-- Content of the view routed -->
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <router-view></router-view>
+    </div>
+    
+  </div>
 </template>
 
 <script>
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
 import Button from '../components/reusable/Button.vue';
+
+
+const user = [
+  {name: 'Axel'},
+  {name: 'Eric'},
+  {name: 'Yael'},
+  {name: 'Anojan'},
+]
+const navigation = [
+  { name: 'Axel', href: '/portfolios/axel', current: false },
+  { name: 'Eric', href: '/portfolios/eric', current: false },
+  { name: 'Home', href: '/', current: true },
+  { name: 'Yael', href: '/portfolios/yael', current: false },
+  { name: 'Anojan', href: '/portfolios/anojan', current: false },
+]
 export default{
+  name:"PortfolioLayout",
     components: {
     Button,
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    BellIcon,
+    MenuIcon,
+    XIcon,
   },
+  setup(){
+    return{
+      user,
+      navigation,
+    }
+    
+  }
 }
 </script>
 
