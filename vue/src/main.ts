c167f9cdb5d02b83d25e88e680ebbd36081920e7
YaelDonat <donatyael@gmail.com>
2022-03-06 17:42:51 +0100

    portfolio yael

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -3,8 +3,16 @@ import store from './store'
 import router from './router'
 import './input.css'
 import App from './App.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas, fab, far);
 
 createApp(App)
+    .component('fa', FontAwesomeIcon)
     .use(store)
     .use(router)
     .mount('#app')
