bb721dbd90739b7b0cec120430b7ce3a493561e7
EricBarnet <70975601+EricBarnet@users.noreply.github.com>
2022-03-07 20:18:34 +0100

    Update Eric.vue (colours update)
    + Ajout d'un dark mode pour le portfolio
    + Rajustement du light mode

--- vue/src/views/Eric.vue
+++ vue/src/views/Eric.vue
@@ -1,27 +1,21 @@
 <template>
-    <div class="bg-dark-gray w-full min-h-screen">
+    <div class="bg-zinc-100 dark:bg-zinc-800 w-full min-h-screen">
         <div
             class="w-full max-w-6xl mx-auto px-4 py-8 flex justify-between md:flex-no-wrap flex-wrap"
         >
             <div class="md:w-1/3 w-full">
                 <header>
-                    <img
-                        src=""
-                        alt="Myself"
-                        class="ml-12"
-                    />
-                    <img
-                        src=""
-                        alt="Logo"
-                        class="mt-4 max-w-sm w-full"
-                    />
+                    <img src="" alt="Myself" class="ml-12" />
+                    <img src="" alt="Logo" class="mt-4 max-w-sm w-full" />
                 </header>
                 <section class="mt-16">
-                    <h3 class="uppercase text-white font-medium text-3xl">
+                    <h3
+                        class="uppercase text-zinc-800 dark:text-zinc-100 font-medium text-3xl"
+                    >
                         Objectifs de carrière
                     </h3>
-                    <div class="h-1 bg-green w-48 my-4"></div>
-                    <p class="text-white">
+                    <div class="h-1 bg-green-600 dark:bg-emerald-200 w-48 my-4"></div>
+                    <p class="text-zinc-900 dark:text-zinc-100">
                         Je suis très motivé à apprendre davantage sur les
                         langages de programmations. Pour ma carrière
                         professionnelle future, je souhaiterais devenir un
@@ -32,11 +26,15 @@
                 </section>
 
                 <section class="mt-16">
-                    <h3 class="uppercase text-white font-medium text-3xl">
+                    <h3
+                        class="uppercase text-zinc-900 dark:text-zinc-100 font-medium text-3xl"
+                    >
                         Compétences
                     </h3>
-                    <div class="h-1 bg-green w-48 my-4"></div>
-                    <ul class="text-white list-disc list-inside">
+                    <div class="h-1 bg-green-600 dark:bg-emerald-200 w-48 my-4"></div>
+                    <ul
+                        class="text-zinc-900 dark:text-zinc-100 list-disc list-inside"
+                    >
                         <li>Langages front-end - HTML, CSS, JavaScript</li>
                         <li>
                             Frameworks front-end - Angular, Bootstrap, Tailwind
@@ -54,29 +52,22 @@
                             Système de gestion de contenu - PrestaShop, Wix,
                             WordPress
                         </li>
-                        <br>
-                        <li>
-                            Français - Natif
-                        </li>
-                        <li>
-                            Anglais - B2+
-                        </li>
-                        <li>
-                            Espagnol - B1
-                        </li>
-                        <li>
-                            Allemand - A1
-                        </li>
-
+                        <br />
+                        <li>Français - Natif</li>
+                        <li>Anglais - B2+</li>
+                        <li>Espagnol - B1</li>
+                        <li>Allemand - A1</li>
                     </ul>
                 </section>
 
                 <section class="mt-16">
-                    <h3 class="uppercase text-white font-medium text-3xl">
+                    <h3
+                        class="uppercase text-zinc-900 dark:text-zinc-100 font-medium text-3xl"
+                    >
                         Contacts
                     </h3>
-                    <div class="h-1 bg-green w-48 my-4"></div>
-                    <div class="text-white">
+                    <div class="h-1 bg-green-600 dark:bg-emerald-200 w-48 my-4"></div>
+                    <div class="text-zinc-900 dark:text-zinc-100">
                         <a
                             href="https://linkedin.com/in/eric-barnet-0210941b9"
                             class="hover:underline flex items-center"
@@ -106,16 +97,18 @@
             </div>
             <div class="md:w-2/4 w-full">
                 <section class="mt-16 md:mt-0">
-                    <h3 class="uppercase text-white font-medium text-3xl">
+                    <h3
+                        class="uppercase text-zinc-900 dark:text-zinc-100 font-medium text-3xl"
+                    >
                         Expériences professionnelles
                     </h3>
-                    <div class="h-1 bg-green w-48 my-4"></div>
+                    <div class="h-1 bg-green-600 dark:bg-emerald-200 w-48 my-4"></div>
 
                     <div class="mt-8">
-                        <h4 class="font-medium text-green text-2xl">
+                        <h4 class="font-medium text-green-600 dark:text-emerald-200 text-2xl">
                             Apprenti développeur web
                         </h4>
-                        <h5 class="text-xl text-green">
+                        <h5 class="text-xl text-green-600 dark:text-emerald-200">
                             <a
                                 href="https://marchesaintpierre.com"
                                 class="hover:underline"
@@ -123,7 +116,9 @@
                             >
                             | <i>NOV 2021 - DEC 2021</i>
                         </h5>
-                        <ul class="text-white list-disc list-inside mt-4">
+                        <ul
+                            class="text-zinc-900 dark:text-zinc-100 list-disc list-inside mt-4"
+                        >
                             <li>
                                 Maquettage du nouveau site web avec Adobe XD
                             </li>
@@ -136,10 +131,10 @@
                     </div>
 
                     <div class="mt-8">
-                        <h4 class="font-medium text-green text-2xl">
+                        <h4 class="font-medium text-green-600 dark:text-emerald-200 text-2xl">
                             Stagiaire développeur web
                         </h4>
-                        <h5 class="text-xl text-green">
+                        <h5 class="text-xl text-green-600 dark:text-emerald-200">
                             <a
                                 href="http://budokanthiais.fr"
                                 class="hover:underline"
@@ -147,7 +142,9 @@
                             >
                             | <i>JAN 2021 - FEV 2021</i>
                         </h5>
-                        <ul class="text-white list-disc list-inside mt-4">
+                        <ul
+                            class="text-zinc-900 dark:text-zinc-100 list-disc list-inside mt-4"
+                        >
                             <li>
                                 Développement front-end (page d'introduction,
                                 page d'accueil, planning)
@@ -164,16 +161,22 @@
                     </div>
                 </section>
                 <section class="mt-16">
-                    <h3 class="uppercase text-white font-medium text-3xl">
+                    <h3
+                        class="uppercase text-zinc-900 dark:text-zinc-100 font-medium text-3xl"
+                    >
                         Formations
                     </h3>
-                    <div class="h-1 bg-green w-48 my-4"></div>
+                    <div class="h-1 bg-green-600 dark:bg-emerald-200 w-48 my-4"></div>
 
                     <div class="mt-8">
-                        <h4 class="font-medium text-green text-2xl">
-                            <a href="https://www.francecompetences.fr/recherche/rncp/31678/" class="hover:underline">BAC +3: TP niv.6 - CDA</a>
+                        <h4 class="font-medium text-green-600 dark:text-emerald-200 text-2xl">
+                            <a
+                                href="https://www.francecompetences.fr/recherche/rncp/31678/"
+                                class="hover:underline"
+                                >BAC +3: TP niv.6 - CDA</a
+                            >
                         </h4>
-                        <h5 class="text-xl text-green">
+                        <h5 class="text-xl text-green-600 dark:text-emerald-200">
                             <a
                                 href="https://cfa-insta.fr/"
                                 class="hover:underline"
@@ -181,7 +184,7 @@
                             >
                             | <i>SEP 2021 - PRESENT</i>
                         </h5>
-                        <p class="text-white">
+                        <p class="text-zinc-900 dark:text-zinc-100">
                             Préparation d'un titre Professionnel de niveau 6
                             (équivalent d'un BAC+3): Concepteur Développeur
                             d'Applications pendant 1 an au sein du CFA INSTA à
@@ -190,10 +193,14 @@
                     </div>
 
                     <div class="mt-8">
-                        <h4 class="font-medium text-green text-2xl">
-                            <a href="https://www.onisep.fr/Ressources/Univers-Formation/Formations/Post-bac/bts-services-informatiques-aux-organisations-option-b-solutions-logicielles-et-applications-metiers" class="hover:underline">BAC +2: BTS SIO opt. SLAM</a>
+                        <h4 class="font-medium text-green-600 dark:text-emerald-200 text-2xl">
+                            <a
+                                href="https://www.onisep.fr/Ressources/Univers-Formation/Formations/Post-bac/bts-services-informatiques-aux-organisations-option-b-solutions-logicielles-et-applications-metiers"
+                                class="hover:underline"
+                                >BAC +2: BTS SIO opt. SLAM</a
+                            >
                         </h4>
-                        <h5 class="text-xl text-green">
+                        <h5 class="text-xl text-green-600 dark:text-emerald-200">
                             <a
                                 href="https://www.lyceedecachan.fr/"
                                 class="hover:underline"
@@ -201,7 +208,7 @@
                             >
                             | <i>SEP 2019 - JUL 2021</i>
                         </h5>
-                        <p class="text-white">
+                        <p class="text-zinc-900 dark:text-zinc-100">
                             Formation de 2 ans pour le BTS (Brevet de Technicien
                             Supérieur) SIO (Services Informatiques aux
                             Organisations) option SLAM (Solutions Logicielles et
@@ -211,28 +218,33 @@
                     </div>
                 </section>
                 <section class="mt-16">
-                    <h3 class="uppercase text-white font-medium text-3xl">
+                    <h3
+                        class="uppercase text-zinc-900 dark:text-zinc-100 font-medium text-3xl"
+                    >
                         Projet en cours
                     </h3>
-                    <div class="h-1 bg-green w-48 my-4"></div>
+                    <div class="h-1 bg-green-600 dark:bg-emerald-200 w-48 my-4"></div>
 
                     <div class="mt-8">
-                        <h4 class="font-medium text-green text-2xl">
-                            Création d'un jeu vidéo + site internet (celui-ci mdr)
+                        <h4 class="font-medium text-green-600 dark:text-emerald-200 text-2xl">
+                            Création d'un jeu vidéo + site internet (celui-ci
+                            mdr)
                         </h4>
-                        <p class="text-white mt-2">
+                        <p class="text-zinc-900 dark:text-zinc-100 mt-2">
                             Avec toute l'équipe, nous sommes en train de créer
-                            un petit jeu sous <a href="https://unity.com/fr" class="underline">Unity</a>. Pour ce faire, un site sera
-                            aussi nécessaire afin d'accueillir le jeu une fois
-                            que tout sera terminé.
+                            un petit jeu sous
+                            <a href="https://unity.com/fr" class="underline"
+                                >Unity</a
+                            >. Pour ce faire, un site sera aussi nécessaire afin
+                            d'accueillir le jeu une fois que tout sera terminé.
                         </p>
                     </div>
 
                     <div class="mt-8">
-                        <h4 class="font-medium text-green text-2xl">
+                        <h4 class="font-medium text-green-600 dark:text-emerald-200 text-2xl">
                             <a>Refonte complet de mon site personnel</a>
                         </h4>
-                        <p class="text-white mt-2">
+                        <p class="text-zinc-900 dark:text-zinc-100 mt-2">
                             Comme projet personnel, je suis en train de refaire
                             ce site
                             <a
@@ -264,14 +276,6 @@ body {
 
 .bg-dark-gray {
     background-color: #272727;
-}
-
-.text-green {
-    color: #29f3c3;
-}
-
-.bg-green {
-    background-color: #29f3c3;
 }
 
 .border-green {
