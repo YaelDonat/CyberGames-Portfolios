4e369a551e4253ef73ae0521fb52869c184df7e7
YaelDonat <donatyael@gmail.com>
2022-06-21 21:06:35 +0200

    netlify

--- server/resources/views/Email/demoEmail.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-# {{ $mailData['title'] }}
-The body of your message.
-@component('mail::button', ['url' => $mailData['url']])
-Button Text
-@endcomponent
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
