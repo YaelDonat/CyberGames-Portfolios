41a14cb9b78ffcdaf65b01ba1c55ebed339efbb2
YaelDonat <donatyael@gmail.com>
2022-06-22 00:38:55 +0200

    fixes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,9 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin:*')
-            ->header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+        header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+        return $next($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,10 +27,10 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::resource('/comments',CommentsController::class);
-    Route::resource('/ratings', RatingsController::class);
-
+    
 });
+Route::resource('/comments',CommentsController::class);
+Route::resource('/ratings', RatingsController::class);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']); 
 
